b295bbce7ec2f39006ad3e9908db394bcb2c15f7
Andrew Owens <andrrowens@gmail.com>
2022-11-28 19:07:13 -0700

    commit tree c5046b91c5caa78fe43751d9ebcaf6b6260b1523

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+
+function writeCards(name) {
+  for (let i = 0; i < names.length; i++) {
+    return `Thank you, ${name}, for the wonderful birthday gift!`;
+  }
+}
+writeCards(names);
